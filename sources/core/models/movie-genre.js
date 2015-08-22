c23933363294998e5233a9f04aa02e7f2d7b0734
Wolfchamane <arturo.mart.diaz@gmail.com>
2015-08-22 20:21:18 +0200

    [feature] Core.ModeMovieGenre

---
+++ sources/core/models/movie-genre.js
@@ -0,0 +1,38 @@
+Core.ModelMovieGenre = Core.extend(
+    {
+        codeMap: {
+            'es_ES':{
+                '00': 'Acción',
+                '01': 'Animación',
+                '02': 'Comedía',
+                '03': 'Drama',
+                '04': 'Documental',
+                '05': 'Fantasía',
+                '06': 'Histórico',
+                '07': 'Ciencia Ficción',
+                '08': 'Terror',
+                '09': 'Suspense',
+                '10': 'Bélico'
+            },
+            'en_EN':{
+                '00': 'Action',
+                '01': 'Animation',
+                '02': 'Comedy',
+                '03': 'Drama',
+                '04': 'Documentary',
+                '05': 'Fantasy',
+                '06': 'Historic',
+                '07': 'Science Fiction',
+                '08': 'Terror',
+                '09': 'Thriller',
+                '10': 'War'
+            }
+        },
+        parse: function(code)
+        {
+            debugger;
+            var lang = Core.Language.getLanguage();
+            return this.codeMap[lang][code];
+        }
+    }
+);
