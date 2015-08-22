5fe6884a244ad6755f9b8599bdebf67275d288c5
Wolfchamane <arturo.mart.diaz@gmail.com>
2015-08-22 20:21:01 +0200

    [fix] update Core.ModelMovies [feature] parse movies genres

--- sources/core/models/movies.js
+++ sources/core/models/movies.js
@@ -3,6 +3,29 @@ Core.ModelMovies = Core.RequestManager.extend(
         movies: null,
         requestParams: null,
         url: '[[HOST]]backend/getMovies.php',
-        method: 'GET'
+        method: 'GET',
+        _parseMoviesGenre: function()
+        {
+            var movies = this.movies;
+            movies.forEach(
+                function(movie)
+                {
+                    movies[movies.lastIndexOf(movie)].genre = Core.ModelMovieGenre.parse(movie.genre);
+                }
+            );
+        },
+        getMovies: function()
+        {
+            var self = this;
+            this._fetch().then(
+                function()
+                {
+                    if (self.movies && self.movies.length)
+                    {
+                        self._parseMoviesGenre();
+                    }
+                }
+            );
+        }
     }
 );
