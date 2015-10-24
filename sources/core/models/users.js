bdf4582b74844e6bd933575822f526eeb40c6f45
Wolfchamane <arturo.mart.diaz@gmail.com>
2015-10-24 21:50:42 +0200

    [feature] users interface

---
+++ sources/core/models/users.js
@@ -0,0 +1,66 @@
+/**
+ * Interface for USERS @ BACKEND
+ *
+ * @namespace   Core
+ * @class       Core.ModelUsers
+ * @extends     Core.RequestManager
+ * @author      Arturo Martínez
+ * @since       2015-10-24
+ */
+Core.ModelUsers = Core.RequestManager.extend
+(
+    {
+        /**
+         * Stores current user
+         * @property    user
+         * @type        {Object}
+         */
+        user            : null,
+        /**
+         * Service URL
+         * @property    url
+         * @type        {String}
+         */
+        url             : '[[HOST]]users.php',
+        /**
+         * Service request params for each REST method
+         */
+        requestParams   : {
+            get         : {
+                appKey  : '',
+                email   : '',
+                password: ''
+            }
+        },
+        /**
+         * Keys to store into browser storage
+         * @property    storageKeys
+         * @type        {String[]}
+         */
+        storageKeys     : ['user'],
+        /**
+         * This model name
+         * @property    modelName
+         * @type        {String|user}
+         * @readOnly
+         */
+        modelName       : 'user',
+        /**
+         * Tries to retrieve an user
+         * @method  getUser
+         * @param data {Object} User's login info
+         * @returns {Core.ModelUsers}
+         */
+        getUser         : function(data)
+        {
+            if (data && (typeof data['email'] !== 'undefined') && (typeof data['password'] !== 'undefined'))
+            {
+                this.doRequest('get', data)
+                    .then(function(){
+                        this.toStorage();
+                    }.bind(this));
+            }
+            return this;
+        }
+    }
+);
